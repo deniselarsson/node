20f285d5c748b37d8f054c14e15d2377f9b276a0
Denisé Larsson <denisee.larsson@gmail.com>
2021-11-09 22:32:35 +0100

    first commit

---
+++ 1-intro.js
@@ -0,0 +1,12 @@
+// Node.js is an open-source, cross-platform, back-end JavaScript 
+// runtime environment that runs on the V8 engine and executes JavaScript 
+// code outside a web browser.
+
+const amount = 15
+
+if (amount < 500 ){
+    console.log('small number');
+}else{
+    console.log('large number');
+}
+console.log(amount);
---
+++ 2-global.js
@@ -0,0 +1,14 @@
+// GLOBALS  - NO WINDOW and browser access!!!!
+
+// __dirname  - path to current directory
+// __filename - file name
+// require    - function to use modules (CommonJS)
+// module     - info about current module (file)
+// process    - info about env where the program is being executed
+
+//console.log(__dirname) //output: /Users/dl/node
+//console.log(__filename) ///Users/dl/node/2-global.js
+
+setInterval(() => {
+console.log("wait")
+},10000)
